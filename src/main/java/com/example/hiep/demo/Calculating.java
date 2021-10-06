a0c516b9fd12bbfc2d8629e41400ec7047ee0758
Lê Hoàng Hiệp <hieplh@splus-software.com.vn>
2021-10-06 16:40:42 +0700

    update post method

--- src/main/java/com/example/hiep/demo/Calculating.java
+++ src/main/java/com/example/hiep/demo/Calculating.java
@@ -3,6 +3,10 @@ package com.example.hiep.demo;
 import com.example.hiep.IRequestHandler;
 import com.example.hiep.MainVerticle;
 import com.example.hiep.error.HandleErrorVerticle;
+import io.vertx.core.MultiMap;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 
 public class Calculating extends MainVerticle implements IRequestHandler {
@@ -17,13 +21,25 @@ public class Calculating extends MainVerticle implements IRequestHandler {
         String second = context.request().getParam("second");
         String operater = context.request().getParam("operater");
 
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(operater);
+        String result = calculate(first, second, operater);
+        context.response().end(String.valueOf(result));
+    }
 
+    private void doPost(RoutingContext context) {
+        Buffer buffer = context.getBody();
+        JsonObject json = new JsonObject(buffer);
+        String first = json.getString("first");
+        String second = json.getString("second");
+        String operater = json.getString("operater");
+
+        String result = calculate(first, second, operater);
+        context.response().end(String.valueOf(result));
+    }
+
+    private String calculate(String first, String second, String operater) {
         double a;
         double b;
-        String error = null;
+        String result;
         try {
             a = Double.parseDouble(first);
         } catch (NumberFormatException e) {
@@ -35,45 +51,42 @@ public class Calculating extends MainVerticle implements IRequestHandler {
             b = 0;
         }
         if (!operater.matches("^(add|subtract|multiply|divide)$")) {
-            error = "The operator is not correct.\n"
+            result = "The operator is not correct.\n"
                     + "Only add, subtract, multiply, devide (+, -, *, /)";
+        } else {
+            switch (operater) {
+                case ADD:
+                    result = String.valueOf(a + b);
+                    break;
+                case SUBTRACT:
+                    result = String.valueOf(a - b);
+                    break;
+                case MULTIPLY:
+                    result = String.valueOf(a * b);
+                    break;
+                case DEVIDE:
+                    if (b == 0) {
+                        result = "The devided number (second) must not be 0";
+                    } else {
+                        result = String.valueOf(a / b);
+                    }
+                    break;
+                default:
+                    result = "The operator is not correct.\n"
+                            + "Only add, subtract, multiply, devide (+, -, *, /)";
+            }
         }
 
-        double result = 0;
-        switch (operater) {
-            case ADD:
-                result = a + b;
-                break;
-            case SUBTRACT:
-                result = a - b;
-                break;
-            case MULTIPLY:
-                result = a * b;
-                break;
-            case DEVIDE:
-                if (b == 0) {
-                    error = "The devided number (second) must not be 0";
-                } else {
-                    result = a / b;
-                }
-                break;
-            default:
-                error = "The operator is not correct.\n"
-                        + "Only add, subtract, multiply, devide (+, -, *, /)";
-        }
-        
-        System.out.println(error);
-        if (error != null) {
-            context.response().end(String.valueOf(error));
-        } else {
-            context.response().end(String.valueOf(result));
-        }
+        return String.valueOf(result);
     }
 
     @Override
     public void handleRequest() {
-        ROUTER.get("/calculate/:operater/:first/:second")
+        ROUTER.route(HttpMethod.GET, "/calculate/:operater/:first/:second")
                 .handler(context -> doGet(context))
+                .failureHandler(f -> new HandleErrorVerticle().redirectError404(f));
+        ROUTER.route(HttpMethod.POST, "/calculate")
+                .handler(context -> doPost(context))
                 .failureHandler(f -> new HandleErrorVerticle().redirectError404(f));
     }
 }
